1f58fc4ed0a7542c8372dc006a724b352763d5e1
Thulasimanikandan <21180063+thulasimanikandan-r@users.noreply.github.com>
2018-05-04 13:32:33 +0530

    First WeatherMan Application Base Code Commit

---
+++ app/src/main/java/com/mani/weather/weatherman/common/util/NetworkUtil.java
@@ -0,0 +1,67 @@
+package com.mani.weather.weatherman.common.util;
+
+import android.content.Context;
+import android.net.Uri;
+import android.util.Log;
+
+import com.mani.weather.weatherman.core.application.AppConstant;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
+
+/**
+ * Created by thulasimanikandan_ra on 04-05-2018
+ */
+public class NetworkUtil {
+
+    private static String QUERY_PARAM = "q";
+    private static String APP_ID_PARAM = "appid";
+    private static String TAG = AppConstant.LOG_TAG;
+
+
+    private static URL buildUrlWithLocationQuery(String locationQuery) {
+        Uri weatherQueryUri = Uri.parse(AppConstant.WEATHER_MAN_BASE_URL).buildUpon()
+                .appendQueryParameter(QUERY_PARAM, locationQuery)
+                .appendQueryParameter(APP_ID_PARAM, AppConstant.API_KEY)
+                .build();
+
+        try {
+            URL weatherQueryUrl = new URL(weatherQueryUri.toString());
+            Log.v(TAG, "URL: " + weatherQueryUrl);
+            return weatherQueryUrl;
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static URL getUrl(Context context){
+        String location = "Bangalore";
+        return buildUrlWithLocationQuery(location);
+    }
+
+    public static String getResponseFromHttpUrl(URL url) throws IOException {
+        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+        try {
+            InputStream in = urlConnection.getInputStream();
+
+            Scanner scanner = new Scanner(in);
+            scanner.useDelimiter("\\A");
+
+            boolean hasInput = scanner.hasNext();
+            String response = null;
+            if (hasInput) {
+                response = scanner.next();
+            }
+            scanner.close();
+            return response;
+        } finally {
+            urlConnection.disconnect();
+        }
+    }
+
+}
