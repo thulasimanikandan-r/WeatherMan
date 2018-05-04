ff2860668ad1fca177e99ace22b5ee4347e9804b
Thulasimanikandan <21180063+thulasimanikandan-r@users.noreply.github.com>
2018-05-04 15:11:08 +0530

    WeatherMan -  Added log_tag and weather_man_base_url constants

--- app/src/main/java/com/mani/weather/weatherman/core/application/AppConstant.java
+++ app/src/main/java/com/mani/weather/weatherman/core/application/AppConstant.java
@@ -4,4 +4,9 @@ package com.mani.weather.weatherman.core.application;
  * Created by thulasimanikandan_ra on 04-05-2018
  */
 public class AppConstant {
+
+    public static final String LOG_TAG = "WeatherMan";
+
+   //WeatherMan Base URL
+    public static final String WEATHER_MAN_BASE_URL = "http://samples.openweathermap.org";
 }
