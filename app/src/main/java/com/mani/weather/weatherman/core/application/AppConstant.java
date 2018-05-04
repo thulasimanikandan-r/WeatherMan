b32e6c565855b6accafb95ea4bfce03544882ff1
Thulasimanikandan <21180063+thulasimanikandan-r@users.noreply.github.com>
2018-05-04 18:23:22 +0530

    WeatherMan - Added projection for content provider and their int constants

--- app/src/main/java/com/mani/weather/weatherman/core/application/AppConstant.java
+++ app/src/main/java/com/mani/weather/weatherman/core/application/AppConstant.java
@@ -1,5 +1,7 @@
 package com.mani.weather.weatherman.core.application;
 
+import com.mani.weather.weatherman.common.data.WeatherContract;
+
 /**
  * Created by thulasimanikandan_ra on 04-05-2018
  */
@@ -7,6 +9,29 @@ public class AppConstant {
 
     public static final String LOG_TAG = "WeatherMan";
 
-   //WeatherMan Base URL
+    //WeatherMan Base URL
     public static final String WEATHER_MAN_BASE_URL = "http://samples.openweathermap.org";
+
+
+    public static final String[] WEATHER_DETAIL_PROJECTION = {
+            WeatherContract.WeatherEntry.COLUMN_DATE,
+            WeatherContract.WeatherEntry.COLUMN_MAX_TEMP,
+            WeatherContract.WeatherEntry.COLUMN_MIN_TEMP,
+            WeatherContract.WeatherEntry.COLUMN_HUMIDITY,
+            WeatherContract.WeatherEntry.COLUMN_PRESSURE,
+            WeatherContract.WeatherEntry.COLUMN_WIND_SPEED,
+            WeatherContract.WeatherEntry.COLUMN_DEGREES,
+            WeatherContract.WeatherEntry.COLUMN_WEATHER_ID
+    };
+
+
+    public static final int INDEX_WEATHER_DATE = 0;
+    public static final int INDEX_WEATHER_MAX_TEMP = 1;
+    public static final int INDEX_WEATHER_MIN_TEMP = 2;
+    public static final int INDEX_WEATHER_HUMIDITY = 3;
+    public static final int INDEX_WEATHER_PRESSURE = 4;
+    public static final int INDEX_WEATHER_WIND_SPEED = 5;
+    public static final int INDEX_WEATHER_DEGREES = 6;
+    public static final int INDEX_WEATHER_CONDITION_ID = 7;
+
 }
