c61b187814759488f9b2fc259cbcb8faf94e52ca
Thulasimanikandan <21180063+thulasimanikandan-r@users.noreply.github.com>
2018-05-07 18:28:13 +0530

    WeatherMan -  added content provider data

--- app/src/main/java/com/mani/weather/weatherman/common/data/WeatherProvider.java
+++ app/src/main/java/com/mani/weather/weatherman/common/data/WeatherProvider.java
@@ -101,12 +101,6 @@ public class WeatherProvider extends ContentProvider {
                 int rowsInserted = 0;
                 try {
                     for (ContentValues value : values) {
-                        /*long weatherDate =
-                                value.getAsLong(WeatherContract.WeatherEntry.COLUMN_DATE);
-                        if (!WeatherManUtils.isDateNormalized(weatherDate)) {
-                            throw new IllegalArgumentException("Date must be normalized to insert");
-                        }*/
-
                         long _id = db.insert(WeatherContract.WeatherEntry.TABLE_NAME, null, value);
                         if (_id != -1) {
                             rowsInserted++;
