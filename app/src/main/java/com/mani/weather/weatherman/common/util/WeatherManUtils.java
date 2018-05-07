9076b8416eb4e2bf7a68bdaeca3116fd82ea3acd
Thulasimanikandan <21180063+thulasimanikandan-r@users.noreply.github.com>
2018-05-07 18:27:12 +0530

    WeatherMan - URL Change from old sample to new real api call

--- app/src/main/java/com/mani/weather/weatherman/common/util/WeatherManUtils.java
+++ app/src/main/java/com/mani/weather/weatherman/common/util/WeatherManUtils.java
@@ -1,5 +1,11 @@
 package com.mani.weather.weatherman.common.util;
 
+import android.content.Context;
+import android.util.Log;
+
+import com.mani.weather.weatherman.R;
+import com.mani.weather.weatherman.core.application.AppConstant;
+
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -7,14 +13,80 @@ import java.util.concurrent.TimeUnit;
  */
 public class WeatherManUtils {
 
-    public static final long DAY_IN_MILLIS = TimeUnit.DAYS.toMillis(1);
+       public static String getFormattedWind(Context context, float windSpeed, float degrees) {
+        int windFormat = R.string.format_wind_kmh;
 
-    public static boolean isDateNormalized(long millisSinceEpoch) {
-        boolean isDateNormalized = false;
-        if (millisSinceEpoch % DAY_IN_MILLIS == 0) {
-            isDateNormalized = true;
+        String direction = "Unknown";
+        if (degrees >= 337.5 || degrees < 22.5) {
+            direction = "N";
+        } else if (degrees >= 22.5 && degrees < 67.5) {
+            direction = "NE";
+        } else if (degrees >= 67.5 && degrees < 112.5) {
+            direction = "E";
+        } else if (degrees >= 112.5 && degrees < 157.5) {
+            direction = "SE";
+        } else if (degrees >= 157.5 && degrees < 202.5) {
+            direction = "S";
+        } else if (degrees >= 202.5 && degrees < 247.5) {
+            direction = "SW";
+        } else if (degrees >= 247.5 && degrees < 292.5) {
+            direction = "W";
+        } else if (degrees >= 292.5 && degrees < 337.5) {
+            direction = "NW";
         }
 
-        return isDateNormalized;
+        return String.format(context.getString(windFormat), windSpeed, direction);
+    }
+
+
+    public static String formatTemperature(Context context, double temperature) {
+
+        temperature = celsiusToFahrenheit(temperature);
+
+        int temperatureId = R.string.format_temperature;
+        return String.format(context.getString(temperatureId), temperature);
+    }
+
+
+    private static double celsiusToFahrenheit(double temperatureInCelsius) {
+        double temperatureInFahrenheit = (temperatureInCelsius * 1.8) + 32;
+        return temperatureInFahrenheit;
+    }
+
+
+    public static int getImagesForWeatherCondition(int weatherId) {
+
+        if (weatherId >= 200 && weatherId <= 232) {
+            return R.drawable.art_snow;
+        } else if (weatherId >= 300 && weatherId <= 321) {
+            return R.drawable.art_light_rain;
+        } else if (weatherId >= 500 && weatherId <= 504) {
+            return R.drawable.art_rain;
+        } else if (weatherId == 511) {
+            return R.drawable.art_snow;
+        } else if (weatherId >= 520 && weatherId <= 531) {
+            return R.drawable.art_rain;
+        } else if (weatherId >= 600 && weatherId <= 622) {
+            return R.drawable.art_snow;
+        } else if (weatherId >= 701 && weatherId <= 761) {
+            return R.drawable.art_fog;
+        } else if (weatherId == 761 || weatherId == 771 || weatherId == 781) {
+            return R.drawable.art_storm;
+        } else if (weatherId == 800) {
+            return R.drawable.art_clear;
+        } else if (weatherId == 801) {
+            return R.drawable.art_light_clouds;
+        } else if (weatherId >= 802 && weatherId <= 804) {
+            return R.drawable.art_clouds;
+        } else if (weatherId >= 900 && weatherId <= 906) {
+            return R.drawable.art_storm;
+        } else if (weatherId >= 958 && weatherId <= 962) {
+            return R.drawable.art_storm;
+        } else if (weatherId >= 951 && weatherId <= 957) {
+            return R.drawable.art_clear;
+        }
+
+        Log.e(AppConstant.LOG_TAG, "Unknown Weather: " + weatherId);
+        return R.drawable.art_storm;
     }
 }
