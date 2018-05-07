3de53a7a4e55afbdee0b958c26e654b56aa309a1
Thulasimanikandan <21180063+thulasimanikandan-r@users.noreply.github.com>
2018-05-07 21:00:41 +0530

    WeatherMan - Added for saving the location in the shared preferences.

--- app/src/main/java/com/mani/weather/weatherman/core/ui/SettingFragment.java
+++ app/src/main/java/com/mani/weather/weatherman/core/ui/SettingFragment.java
@@ -1,9 +1,11 @@
 package com.mani.weather.weatherman.core.ui;
 
 
+import android.app.Activity;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.preference.CheckBoxPreference;
+import android.support.v7.preference.EditTextPreference;
 import android.support.v7.preference.ListPreference;
 import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceFragmentCompat;
@@ -67,11 +69,16 @@ public class SettingFragment extends PreferenceFragmentCompat implements
 
     @Override
     public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
-       // Activity activity = getActivity();
+        Activity activity = getActivity();
 
         if (key.equals(getString(R.string.pref_location_key))) {
-            WeatherManSyncUtil.startImmediateSync(getContext());
+            WeatherManSyncUtil.startImmediateSync(activity);
         }
-
+        Preference preference = findPreference(key);
+        if (null != preference) {
+            if (!(preference instanceof CheckBoxPreference)) {
+                setPreferenceSummary(preference, sharedPreferences.getString(key, ""));
+            }
+        }
     }
 }
